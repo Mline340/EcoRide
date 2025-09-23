f07c4781b5d7b8a1e8828ef72dc14f213533a1e2
Emeline Brun <emeline_3010@hotmail.fr>
2025-09-23 13:56:31 +0200

    ajout page singup.js pour confirmation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoit", "Covoiturages", "/pages/covoiturage/covoit.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/historique", "Historique trajets", "/pages/covoiturage/historique.html"),
     new Route("/annonce", "Publier une annonce", "/pages/covoiturage/annonce.html"),
